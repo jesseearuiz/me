36d150a2dea634974a9a7f5e3be1fb7e966960de
Andy Deng <denga@bc.edu>
2022-11-27 20:09:36 -0500

    one two three

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,9 +3,8 @@
 export default function Navbar() {
   let inner = ``;
   inner += `<div class="nav-container">
-    <a class="nav-home" href="#">Home</a>
+    <a class="nav-home" href="#">Andy Deng</a>
     <div class="nav-group">
-      <a class="nav-element" href="#">About</a>
       <a class="nav-element" href="#p">Projects</a>
       <a class="nav-element" href="#e">Experience</a>
       <a class="nav-element" href="#c">Contact</a>
